7fcd7858d1b3605c63b1428fd60fb1f9b3fe1d29
Matilde <matilde.rodrigues@edu.devinci.fr>
2021-01-06 09:38:09 +0100

    [FEAT] PlayerMouvement is clamped

--- Juicyness/Assets/Scripts/SpaceShip/SpaceShipMouvement.cs
+++ Juicyness/Assets/Scripts/SpaceShip/SpaceShipMouvement.cs
@@ -7,12 +7,16 @@ public class SpaceShipMouvement : MonoBehaviour
     [SerializeField] private float speed = 2;
     [SerializeField] private float life = 3;
     [SerializeField] private Text lifeText;
+    private Vector3 screenBounds;
+    private float playerWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         GameManager.instance.SetPlayer(gameObject);
         lifeText.text = "Life : " + life;
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        playerWidth = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -28,6 +32,8 @@ public class SpaceShipMouvement : MonoBehaviour
             {
                 transform.position -= Vector3.right * speed * Time.deltaTime;
             }
+            float x = Mathf.Clamp(transform.position.x, screenBounds.x + playerWidth, -screenBounds.x - playerWidth);
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
         }
     }
 
